2aae7c4872b9236aaf165b3505f7aa3b7b5edf4e
thejefftrent <thejefftrent@gmail.com>
2019-03-31 15:50:31 -0600

    minimal bot test

---
+++ auth.py
@@ -0,0 +1,1 @@
+discordToken = "NTYyMDIwMTU0MzQ3ODgwNDQ5.XKEtsg.j3TEzyplzPIkEwN3Nz8YGdujffg"
---
+++ rawrbot.py
@@ -0,0 +1,17 @@
+import auth
+import discord
+
+client = discord.Client();
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        if message.content.startswith('+hello'):
+            await message.channel.send('Hello!')
+        else
+            return
+client.run(discordToken)
