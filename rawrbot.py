cd99f2bbf3a1c669ccd2b13c2dd3c5854db81bce
thejefftrent <thejefftrent@gmail.com>
2019-03-31 18:15:05 -0600

    no command

--- rawrbot.py
+++ rawrbot.py
@@ -9,9 +9,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author != client.user:
+    if message.author != client.user && message.content.startswith('+'):
         if message.content.startswith('+hello'):
             await message.channel.send('Hello! {0.name}'.format(message.author))
+        else:
+            command = message.content.split(" ")[0][1:]
+            await message.channel.send("Sorry, " + command + " is not a valid command")
+
     else:
         return
+
+
 client.run(auth.discordToken)
