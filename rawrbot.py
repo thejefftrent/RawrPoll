7499f5a8825c7522b33bca5774ed5f1b6d692bd4
thejefftrent <thejefftrent@gmail.com>
2019-03-31 21:33:15 -0600

    the beginning of polls

--- rawrbot.py
+++ rawrbot.py
@@ -2,9 +2,16 @@ import auth
 import discord
 from discord.ext import commands
 
+############
+# INITIATE #
+############
 bot = commands.Bot(command_prefix='+');
 
+polls = list()
 
+##########
+# EVENTS #
+##########
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
@@ -15,12 +22,34 @@ async def on_message(message):
     if not message.author.bot:
         await bot.process_commands(message)
 
+
+
+############
+# COMMANDS #
+############
+
 @bot.command()
 async def test(ctx):
     await ctx.send("Hello!")
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("PONG! " + str(bot.latency*1000)[:4] + " ms")
+    await ctx.send("PONG! " + str(bot.latency*1000)[:6] + "ms")
 
+@bot.command()
+async def poll(ctx, arg):
+    polls.append(await ctx.send(arg))
+    print(polls[len(polls)-1].id)
+
+@bot.command()
+async def add(ctx, arg):
+    polls[0].add_reaction('{crit}')
+    pass
+
+@bot.command()
+async def end(ctx):
+    pass
+###########################
+### LETS RUN THIS THING ###
+###########################
 bot.run(auth.discordToken)
