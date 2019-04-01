52fb545b6c516731bc1b7deeda9975b94fa46d66
thejefftrent <thejefftrent@gmail.com>
2019-03-31 22:40:16 -0600

    test

--- rawrbot.py
+++ rawrbot.py
@@ -49,9 +49,11 @@ async def poll(ctx, arg):
 async def add(ctx, arg1, arg2, id = 0):
     print(arg1 + " " + arg2)
     emoji = get(bog.get_all_emojis(), name=arg2)
+    print(emoji)
     embed = polls[id].embeds[0]
     embed.add_field(name=emoji, value=arg1, inline=False)
-    await polls[id].add_reaction(emoji)
+    #await polls[id].add_reaction(emoji)
+    #await polls[id].edit(embed=embed)
 
 @bot.command()
 async def end(ctx):
