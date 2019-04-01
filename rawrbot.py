9a36c042db4011a11660c2fcd90830ca9c738fb2
thejefftrent <thejefftrent@gmail.com>
2019-03-31 20:45:45 -0600

    ...

--- rawrbot.py
+++ rawrbot.py
@@ -9,8 +9,8 @@ bot = commands.Bot(command_prefix='+');
 #@client.event
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("with the API"))
+    print('We have logged in as {0.user}'.format(bot))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("with the API"))
 
 #@client.event
 @bot.event
@@ -23,4 +23,4 @@ async def test(ctx):
     await ctx.send("Hello!")
 
 
-client.run(auth.discordToken)
+bot.run(auth.discordToken)
