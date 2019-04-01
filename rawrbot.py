09360ab112108ac9c201699b90db6fdee1693f0e
thejefftrent <thejefftrent@gmail.com>
2019-03-31 20:44:06 -0600

    ??

--- rawrbot.py
+++ rawrbot.py
@@ -3,17 +3,20 @@ import discord
 from discord.ext import commands
 
 
-client = discord.Client();
+#client = discord.Client();
 bot = commands.Bot(command_prefix='+');
 
-@client.event
+#@client.event
+@bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(status=discord.Status.online, activity=discord.Game("with the API"))
 
-@client.event
+#@client.event
+@bot.event
 async def on_message(message):
-     await bot.process_commands(message)
+    if not message.author.bot:
+        await bot.process_commands(message)
 
 @bot.command()
 async def test(ctx):
